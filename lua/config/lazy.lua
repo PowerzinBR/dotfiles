81a60f463c222bd435f0f2a19316bb0d13841f08
PowerDev <140966320+PowerzinBR@users.noreply.github.com>
2024-03-17 14:30:39 -0300

    Update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,7 +14,7 @@ require("lazy").setup({
       }
     },
     { import = "lazyvim.plugins.extras.linting.eslint" },
-		{ import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
